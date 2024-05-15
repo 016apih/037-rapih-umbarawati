f1557e3d47ad54eed79219d1c9fce5f273500858
Apih <umbarawatirapih@gmail.com>
2024-05-15 08:46:33 +0700

    paste materi slicing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,40 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $products = [
+        [
+            'name' => 'Vanilla Latte',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, nihil?',
+            'price' => 12,
+            'isFeatured' => true,
+            'rating' => 4.2,
+            'image' => './assets/images/product-1.png',
+            'tags' => ['Hot', 'Cold']   
+        ],
+        [
+            'name' => 'Ekspresso',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, nihil?',
+            'price' => 14,
+            'isFeatured' => false,
+            'rating' => 4.5,
+            'image' => './assets/images/product-2.png',
+            'tags' => ['Sweet', 'Sour']   
+        ],
+        [
+            'name' => 'Hazelnut Latte',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, nihil?',
+            'price' => 24,
+            'isFeatured' => false,
+            'rating' => 4.8,
+            'image' => './assets/images/product-3.png',
+            'tags' => ['Salty', 'Crunchy']
+        ],
+    ];
+
+    return view('homepage', compact('products'));
+});
+
+Route::get('/delivery', function () {
+    return view('delivery');
 });
